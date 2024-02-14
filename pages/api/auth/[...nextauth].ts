348e2f649a82d2487692a0442dc3ce1126fc099b
zakaria bouregbi <zakariabouregbidev@gmail.com>
2024-02-14 13:08:12 +0100

    fix NextAuth bugs

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -1,19 +1,18 @@
 import NextAuth from "next-auth"
 import GoogleProvider from "next-auth/providers/google"
+import Providers from "next-auth/providers"
 import { PrismaAdapter } from "@next-auth/prisma-adapter"
 import { PrismaClient } from "@prisma/client"
 
-
 const prisma = new PrismaClient()
 
-
 export default NextAuth({
-    adapter: PrismaAdapter(prisma),
-    providers: [
-      GoogleProvider({
-        clientId: process.env.GOOGLE_CLIENT_ID as string,
-        clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
-      }),
-    ],
-    
-  })
+  providers: [
+    GoogleProvider({
+      clientId: process.env.GOOGLE_CLIENT_ID as string,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
+    }),
+  ],
+  adapter: PrismaAdapter(prisma),
+})
+  
