c2aa86bcc35379fc31a189263ca8436731963211
zakaria bouregbi <zakariabouregbidev@gmail.com>
2024-02-14 14:50:41 +0100

    add Stripe To User Account

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -3,6 +3,7 @@ import GoogleProvider from "next-auth/providers/google"
 import Providers from "next-auth/providers"
 import { PrismaAdapter } from "@next-auth/prisma-adapter"
 import { PrismaClient } from "@prisma/client"
+import Stripe from "stripe"
 
 const prisma = new PrismaClient()
 
@@ -12,7 +13,34 @@ export default NextAuth({
       clientId: process.env.GOOGLE_CLIENT_ID as string,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
     }),
+
   ],
   adapter: PrismaAdapter(prisma),
+  events:{
+    createUser: async({user}) =>{
+      const stripe = new Stripe(process.env.STRIPE_SECRET_KEY as string,{
+
+        apiVersion: '2022-11-15'
+
+      })
+      if (user.name && user.email){
+      const costumer = await stripe.customers.create({
+        email: user.email,
+        name:user.name,
+
+      })
+      
+    await prisma.user.update({
+
+      where : {id: user.id},
+      data:{stripeCustumerId: costumer.id},
+
+    })
+    }
+
+    }
+
+  }
+
 })
   
